95c61a17cb7db8fad164f00f324ad02029c63b88
trevortuepah <trevortuepah@gmail.com>
2022-08-27 14:11:58 -0700

    use process env port if it exists

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,15 @@ import { initRoutes } from './routes';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const handler = () => {
   app.use(cors());
   app.use(express.urlencoded( {extended: true }));
   app.use(express.json());
 
-  app.listen(port, () => {
-    console.log(`Sudoku running on port ${port}`);
+  app.listen(PORT, () => {
+    console.log(`Sudoku running on http://localhost:${PORT}`);
   });
   initRoutes(app);
 }
